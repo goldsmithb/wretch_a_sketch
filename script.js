4f0bec3cf7e1f3bc13e18449ca9607c73a81e6ce
Bradley Goldsmith <bradleygoldsmith14@gmail.com>
2022-10-08 22:38:52 -0400

    allow custom number of pixels

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
+let size = prompt("How many pixels do you want?");
+if (size > 50) size = 50;
+console.log(size);
+
 const container = document.getElementById('container');
-const divs = Array(16);
+const divs = Array(size*size);
 for (let i = 0; i < divs.length; i++) {
   const temp = document.createElement('div');
 
   temp.classList.add('cell');
-  let width = (container.offsetHeight / 4)  + "px";
+  let width = (container.offsetWidth / size)  + "px";
   temp.style.width = width;
   temp.style.height = width;
-  temp.addEventListener('mouseover', () => {
-    temp.style.backgroundColor = "black";
-  });
+  temp.addEventListener('mouseover', () => temp.style.backgroundColor = "black");
 
   container.appendChild(temp);
   divs[i] = temp;
