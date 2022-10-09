ec88308cb0c2054af4fcfdffce9049e3e6500b96
Bradley Goldsmith <bradleygoldsmith14@gmail.com>
2022-10-08 22:23:24 -0400

    use mouseover js event instead

--- script.js
+++ script.js
@@ -2,11 +2,15 @@ const container = document.getElementById('container');
 const divs = Array(16);
 for (let i = 0; i < divs.length; i++) {
   const temp = document.createElement('div');
+
   temp.classList.add('cell');
-  console.log(container.offsetWidth);
   let width = (container.offsetHeight / 4)  + "px";
   temp.style.width = width;
   temp.style.height = width;
+  temp.addEventListener('mouseover', () => {
+    temp.style.backgroundColor = "black";
+  });
+
   container.appendChild(temp);
   divs[i] = temp;
 }
